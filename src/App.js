d95b6f0849984c628d1e5f7eda5d3486d5f152a1
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-03-25 14:40:28 -0400

    instala o pacote react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
-import {useState} from 'react'
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
 
 function App() {
-  const [nome,setNome] = useState()
   return (
     <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome= {setNome} />
-      <Saudacao nome={nome} />
     </div>
     
   )
