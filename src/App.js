60ce1c9d81a38b0f0f2b0b96b36e78235cb07d7a
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-03-25 15:39:48 -0400

    cria Navbar e Footer e adiciona rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
+  return (//dentro de router eu consigo definir as views (rotas e os componentes padrão que vao ser repetidos)
+    <Router> 
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/contato" element={<Contato/>}></Route>
+        <Route path="/empresa" element={<Empresa/>}></Route>
+      </Routes>
+      <Footer/>
+
+    </Router>
     
   )
 }
