815cb5187935ae9cb0625968c74d2f4b42e0ce48
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-03-25 12:25:41 -0400

    metodos por props

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,15 +1,26 @@
+import Button from "./evento/Button"
+
 function Evento() {
 
     function meuEvento(){
-        console.log('Opa, fui ativado!')
+        console.log(`Ativando o primeiro evento`)
+    }
+
+    function segundoEvento(){
+        console.log(`Ativando o segundo evento`)
     }
 
     return (
         <div>
             <p>Clique para disparar um evento</p>
-            <button onClick={meuEvento} >Ativar!</button>
+            <Button event={meuEvento} text ="Primeiro Evento"/> 
+            <Button event={segundoEvento} text ="Segundo Evento"/> 
+            
         </div>
     )
+
+    ////se colocar meuEvento(), o componente entende que já é pra executar a função e ja da o console log
+    //para q o componente entenda que a funcao so deve ser realizada depois, a gente passa dessa forma 'meuEvento' sem os parenteses
 }
 
 export default Evento
